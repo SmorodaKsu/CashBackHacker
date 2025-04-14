532bb089d854e4966951aa82e9212541d8ddff01
ksu <ksunoonnn@yandex.ru>
2025-04-14 14:54:47 +0300

    Add TestNG tests

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    private final CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testRemain() {
+        Assertions Assert;
+        Assert.assertEquals(service.remain(900), 100);
+        Assert.assertEquals(service.remain(1000), 0);
+        Assert.assertEquals(service.remain(1500), 500);
+    }
+}
+
