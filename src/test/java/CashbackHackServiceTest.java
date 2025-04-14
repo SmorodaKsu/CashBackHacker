3eefc90060e87b4fb405817c26b75392ecb42ebc
ksu <ksunoonnn@yandex.ru>
2025-04-14 14:56:57 +0300

    Add JUnit4 tests

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -16,3 +16,18 @@ public class CashbackHackServiceTest {
     }
 }
 
+import org.junit.Assert;
+import org.junit.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    private final CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testRemain() {
+        Assert.assertEquals(100, service.remain(900));
+        Assert.assertEquals(0, service.remain(1000));
+        Assert.assertEquals(500, service.remain(1500));
+    }
+}
